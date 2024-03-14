daa0e929e538302da53ea82d66f7c2964f242fe7
Maksim Akimov <maksimakimov.96@mail.ru>
2024-03-14 20:24:34 +0300

    add m

---
+++ model.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    surname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}')"
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
